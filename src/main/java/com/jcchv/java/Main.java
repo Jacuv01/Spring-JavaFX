128eb666c84683f15ee00763f9eb664aca8940ef
Juan <juan.chavez@autozone.com>
2017-06-21 14:04:15 -0600

    Hello World SpringBoot-JavaFX Example

--- src/main/java/com/jcchv/java/Main.java
+++ src/main/java/com/jcchv/java/Main.java
@@ -5,19 +5,38 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@SpringBootApplication
 public class Main extends Application {
 
+    private ConfigurableApplicationContext configurableApplicationContext;
+    private Parent parent;
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
+    public void init() throws Exception {
+        configurableApplicationContext= SpringApplication.run(Main.class);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
+        fxmlLoader.setControllerFactory(configurableApplicationContext::getBean);
+        parent= fxmlLoader.load();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(parent,800, 600));
+        primaryStage.show();
         primaryStage.setTitle("Spring Java-FX");
-        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void stop() throws Exception {
+        configurableApplicationContext.close();
     }
 }
